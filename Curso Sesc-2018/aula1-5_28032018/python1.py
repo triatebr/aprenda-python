dedcafd00fd1e6c10c884dc20340d72613e38afd
LUBT <triate@gmail.com>
2018-03-29 11:31:42 -0300

    Add files via upload
    Material de aprendizado e exercícios do curso realizado no Sesc Consolação

---
+++ Curso Sesc-2018/aula1-5_28032018/python1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 10:14:26 2018
+
+@author: lucas
+"""
+
+print('Introdução à lógica de programação com Python')
+print('Aula1/528-03-2018')
+hardware='Arduino'
+print(hardware)
+print('Tipos de Variáveis')
+pasteis=2
+print(pasteis)
+coxinhas=12
+print(coxinhas)
+amigos_do_face=523
+print(amigos_do_face)
